6694b074164e143dafe8f6707b2277b9a4f22011
Кирилл <112952627+motorollainlinux@users.noreply.github.com>
2022-11-08 19:36:34 +0700

    ATM complited!

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@
 
 using namespace std;
 
-int UserID, UserBalance, UserPincode, UserChoice;
+int UserID, UserBalance, UserPincode, UserChoice, TransferID;
 char NextSumbol; 
 bool IsValidEnter = false;
 map <int, int> Cards;
@@ -32,20 +32,16 @@ void Files()
      UsersID = fopen("users_id.txt", "r");
      UsersPincode = fopen("pin-codes.txt", "r");
      UsersBalance = fopen("balances.txt", "r");
-     // UsersTransaction = fopen("logs.txt", "w");
-     fscanf(UsersID, "%d", &UserID);
-     fscanf(UsersID, "%*[^/n]");
-     
-     // заполнеие массивов
+     // заполнеие map
      for(int x = 0, y = 0, i = 0; i<=10; i++)
     {
-         fscanf(UsersID, "%d",&x); 
+         fscanf(UsersID, "%d", &x); 
          fscanf(UsersID, "%d", &y);
          Cards[x] = y;
     }
       for(int x = 0, y = 0, i = 0; i<=10; i++)
     {
-         fscanf(UsersPincode, "%d",&x); 
+         fscanf(UsersPincode, "%d", &x); 
          fscanf(UsersPincode, "%d", &y);
          PinCodes[x] = y;
     }
@@ -56,6 +52,13 @@ void Files()
          fscanf(UsersBalance, "%d", &y);
          Balances[x] = y;
     }
+     //
+     fclose(UsersID);
+     fclose(UsersPincode);
+     fclose(UsersBalance);
+     UsersID = fopen("users_id.txt", "r");
+     fscanf(UsersID, "%d", &UserID);
+     fclose(UsersID);
      UserPincode = PinCodes[UserID];
      UserBalance = Balances[UserID];
 } 
@@ -72,8 +75,9 @@ void BalanceUpdate()
          fprintf(UsersBalance, "%d\n", y);
     }
      UserBalance = Balances[UserID];
+     fclose(UsersBalance);
 }
-void LogsWrite(int Function,int TransferID)
+void LogsWrite(int Function)
 {
      string StrTime;
      FILE *UsersTransaction;
@@ -82,15 +86,29 @@ void LogsWrite(int Function,int TransferID)
      tm* TimeNow = localtime(&Time);
      char CTime[128];
      strftime(CTime, sizeof(CTime), "%d.%m.%Y %X", TimeNow);
-          if (Function == 0)
-     fprintf(UsersTransaction, "%s||%d руб снято с карты; ID карты:%d", CTime, UserChoice, UserID);
+     /**/ if (Function == 0)
+     fprintf(UsersTransaction, "%s||%d руб снято с карты; ID карты:%d\n", CTime, UserChoice, UserID);
      else if (Function == 1)
-     fprintf(UsersTransaction, "%s||%d руб зачиленно на карту; ID карты:%d", CTime, UserChoice, UserID);
+     fprintf(UsersTransaction, "%s||%d руб зачиленно на карту; ID карты:%d\n", CTime, UserChoice, UserID);
      else if (Function == 2)
-     fprintf(UsersTransaction, "%s||оплачена мобильная связь с карты; ID карты:%d", CTime, UserID);
+     fprintf(UsersTransaction, "%s||оплачена мобильная связь с карты; ID карты:%d\n", CTime, UserID);
      else if (Function == 3)
-     fprintf(UsersTransaction, "%s||%d руб снято с карты и зачисленно на карту:%d; ID карты:%d", CTime, UserChoice, TransferID, UserID);
+     fprintf(UsersTransaction, "%s||%d руб снято с карты и зачисленно на карту:%d; ID карты:%d\n", CTime, UserChoice, TransferID, UserID);
      else return;
+     fclose(UsersTransaction);
+}
+void CardsUpdate(int q)
+{
+     FILE *UsersID;
+     UsersID = fopen("users_id.txt","w");
+     Cards[UserID] = q;
+     for (int y = 0, i = 27836; i <= 27845; i++)
+     {
+         y = Cards[i];
+         fprintf(UsersID, "%d ", i);
+         fprintf(UsersID, "%d\n", y);
+     }
+     fclose(UsersID);
 }
 //функции банкомата
 void CashOut()
@@ -109,14 +127,19 @@ void CashOut()
          printf("отмена...\n");
          return;
      }
-     } else IsValidEnter = true;
+     } else 
+     {
+         IsValidEnter = true;
+         printf("успешно.\n");
+     }
      } while(!IsValidEnter);
+     IsValidEnter = false;
      if (UserChoice <= UserBalance)
      {
          UserBalance = UserBalance - UserChoice;
          Balances[UserID] = UserBalance;
          BalanceUpdate();
-         LogsWrite(0,0);
+         LogsWrite(0);
      }
 }
 void Replenishment()
@@ -134,12 +157,17 @@ void Replenishment()
          printf("отмена...\n");
          return;
      }
-     } else IsValidEnter = true;
+     } else 
+     {
+         IsValidEnter = true;
+         printf("успешно.\n");
+     }
      } while(!IsValidEnter);
+     IsValidEnter = false;
      UserBalance = UserBalance + UserChoice;
      Balances[UserID] = UserBalance;
      BalanceUpdate();
-     LogsWrite(1,0);
+     LogsWrite(1);
 }
 void PaymentOfMobileConection()
 {
@@ -157,12 +185,15 @@ void PaymentOfMobileConection()
          return;
      }
      } else IsValidEnter = true;
+     } while(!IsValidEnter);
+     IsValidEnter = false;
      /****/ if (UserChoice == 1) 
      { if (UserBalance >= 850)
         {          
-         UserBalance -= 850;
-         Balances[UserID] = UserBalance;
-         BalanceUpdate();
+             UserBalance -= 850;
+             Balances[UserID] = UserBalance;
+             BalanceUpdate();
+             printf("успешно.\n");
         } else printf("недостаточно средств! тариф: 850 руб/мес\n");
      } else if (UserChoice == 2)
      {
@@ -171,6 +202,7 @@ void PaymentOfMobileConection()
              UserBalance -= 400;
              Balances[UserID] = UserBalance;
              BalanceUpdate();
+             printf("успешно.\n");
          } else printf("недостаточно средств! тариф: 400 руб/мес \n");
      } else if (UserChoice == 3)
      {
@@ -179,6 +211,7 @@ void PaymentOfMobileConection()
              UserBalance -= 550;
              Balances[UserID] = UserBalance;
              BalanceUpdate();
+             printf("успешно.\n");
          } else printf("недостаточно средств! тариф: 550 руб/мес\n");
      } else if (UserChoice == 4)
      {
@@ -187,13 +220,49 @@ void PaymentOfMobileConection()
              UserBalance -= 750;
              Balances[UserID] = UserBalance;
              BalanceUpdate();
+             printf("успешно.\n");
          } else printf("недостаточно средств! тариф: 750 руб/мес\n");
      }
-     LogsWrite(2,0);
+     LogsWrite(2);
 }
 void Transfer()
 {
-     printf("inWorck\n");  
+      do{ printf("введие ID человека, котому хотите перевести деньги(0-отмена)\n");
+     if (scanf("%d%c", &TransferID, &NextSumbol) != 2 ||
+     NextSumbol != '\n' || TransferID > 27845 || TransferID < 27836)
+     { 
+         if (!TransferID == 0)
+     {
+         printf("неверное значение, попробйте снова!\n");
+         scanf("%*[^\n]");
+     } else 
+     {
+         printf("отмена...\n");
+         return;
+     }
+     } else if (Cards[TransferID] == 0)
+     {
+         printf("карта пользователя заблокированна!\n");
+         return;
+     } else IsValidEnter = true;
+     }while(!IsValidEnter);
+     IsValidEnter = false;
+     do{ printf("введие сумму, которую хотите перевести\n");
+     if (scanf("%d%c", &UserChoice, &NextSumbol) != 2 ||
+     NextSumbol != '\n' || UserChoice == 0)
+     { 
+         printf("неверное значение, попробйте снова!\n");
+         scanf("%*[^\n]");
+     } else IsValidEnter = true;
+     } 
+     while(!IsValidEnter);
+     if (UserBalance >= UserChoice)
+     {
+         Balances[UserID] -= UserChoice;
+         Balances[TransferID] += UserChoice;
+         BalanceUpdate();
+         LogsWrite(3);
+     }
 }
 void VievBalance()
 {
@@ -202,31 +271,45 @@ void VievBalance()
 int main()
 {
      Files();
-     /**/ printf("%d\n",Balances[UserID]); /**/
-     int CountEnter = 0;
-     do{ printf("Здравствуйте! Введите ваш пин-код.\n");
-     if (scanf("%d%c", &UserChoice, &NextSumbol) != 2 ||
-     NextSumbol != '\n' || UserChoice != UserPincode)
-     { if (CountEnter != 3)
+     if (Cards[UserID] != 1)
      {
+         return 2;
+     }
+     int IsExit = false;
+     int CountEnter = 0;
+     printf("Здравствуйте! Введите ваш пин-код.\n");
+     do{
+     if (scanf("%d%c", &UserChoice, &NextSumbol) != 2 ||
+     NextSumbol != '\n' || UserChoice != UserPincode|| CountEnter == 3)
+     { 
+     
          printf("неверное значение, попробйте снова!\n");
+         printf("у вас осталось %d попыток\n", 3-CountEnter);
          scanf("%*[^\n]");
          CountEnter++;
-     } else return 0;
+         if (CountEnter == 3)
+         {
+             CardsUpdate(0);
+             return 2;
+         }
      } else IsValidEnter = true;
      } while(!IsValidEnter);
      IsValidEnter = false;
      do 
      {
-         printf("сделайте свойвыбор:\n    0-снятие денег\n    1-поплнеие сщёта\n    2-перевод\n    3-оплатить мобильную связь\n    4-посмотреть баланс\n");
+     do 
+     {
+         printf("сделайте свойвыбор:\n    0-снятие денег\n    1-поплнеие сщёта\n    2-оплатить мобильную связь\n    3-перевод\n    4-посмотреть баланс\n    -1-выход\n");
          if (scanf("%d%c", &UserChoice, &NextSumbol) != 2 ||
-         NextSumbol != '\n' || UserChoice < 0 || UserChoice > 4)
+         NextSumbol != '\n' || UserChoice < -1 || UserChoice > 4)
          {
              printf("неверное значение, попробйте снова!\n");
              scanf("%*[^\n]");
          } else IsValidEnter = true;
      } while(!IsValidEnter);
-          if (UserChoice == 0)
+     /***/ if (UserChoice == -1)
+     IsExit = true;
+     else  if (UserChoice == 0)
      CashOut();
      else if (UserChoice == 1)
      Replenishment();
@@ -236,4 +319,6 @@ int main()
      Transfer();
      else if (UserChoice == 4)
      VievBalance();
+     } while(!IsExit);
+     return 0;
 }
